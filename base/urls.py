7fe38b1d446c30f451cbe75ba86bffb907dd9496
Basith P <basithpofficial@gmail.com>
2023-08-28 14:42:19 +0530

    add: task details page

--- base/urls.py
+++ base/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', TaskList.as_view(), name='tasks_list')
+    path('', TaskList.as_view(), name='tasks_list'),
+    path('task/<int:pk>/', TaskDetails.as_view(), name='task_details')
 ]
--- base/views.py
+++ base/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 
 from base.models import Task
 
 
 class TaskList(ListView):
+    model = Task
+    context_object_name = 'tasks'
+
+
+class TaskDetails(DetailView):
     model = Task
     context_object_name = 'tasks'
