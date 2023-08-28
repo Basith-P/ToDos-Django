e374e66a3972b291c048b60f19f79eb3212dc1d2
Basith P <basithpofficial@gmail.com>
2023-08-28 15:58:04 +0530

    feat: delete task

--- base/urls.py
+++ base/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('task/<int:pk>/', TaskDetails.as_view(), name='task_details'),
     path('task/create/', TaskCreate.as_view(), name='create_task'),
     path('task/edit/<int:pk>/', TaskUpdate.as_view(), name='udpate_task'),
+    path('task/delete/<int:pk>/', TaskDelete.as_view(), name='delete_task'),
 ]
